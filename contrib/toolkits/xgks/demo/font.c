4cead09d9a2c990c0fbb439a64a86185e052aef4
Richard <legalize@xmission.com>
2021-02-22 00:42:46 -0700

    clang-format font demo

--- contrib/toolkits/xgks/demo/font.c
+++ contrib/toolkits/xgks/demo/font.c
@@ -41,7 +41,7 @@
 
 #include "demo.h"
 
-Gint    ws_id = 1;
+Gint ws_id = 1;
 
 Gint result;
 
@@ -58,201 +58,230 @@ void SetColor(Gint hash);
 
 int main(int argc, char *argv[])
 {
-        double atof();
-        int i;
-        char *conn = (char *)NULL;
+    double atof();
+    int i;
+    char *conn = (char *) NULL;
 
-        for( i=1; i<argc; i++){
-                if (index( argv[i], ':'))
-                        conn = argv[i];
+    for (i = 1; i < argc; i++)
+    {
+        if (index(argv[i], ':'))
+            conn = argv[i];
         /* Application dependent options here */
-        }
+    }
 
-        gopengks(stdout,0);
-        if ((result = gopenws(ws_id, conn, conn)) != 0)
-                perr(result, "...open_ws");
-        gactivatews(ws_id);
+    gopengks(stdout, 0);
+    if ((result = gopenws(ws_id, conn, conn)) != 0)
+        perr(result, "...open_ws");
+    gactivatews(ws_id);
 
-        gopenws (200, "font.out", "MO");
-        gactivatews (200);
+    gopenws(200, "font.out", "MO");
+    gactivatews(200);
 
-        wsw.xmin = 0.0;
-        wsw.xmax = 1.0;
-        wsw.ymin = 0.0;
-        wsw.ymax = 0.8;
-        gsetwswindow(200, &wsw);
+    wsw.xmin = 0.0;
+    wsw.xmax = 1.0;
+    wsw.ymin = 0.0;
+    wsw.ymax = 0.8;
+    gsetwswindow(200, &wsw);
 
-        wsv.xmin = 0.0;
-        wsv.xmax = 1279.0;
-        wsv.ymin = 0.0;
-        wsv.ymax = 1023.0;
-        gsetwsviewport(200, &wsv);
+    wsv.xmin = 0.0;
+    wsv.xmax = 1279.0;
+    wsv.ymin = 0.0;
+    wsv.ymax = 1023.0;
+    gsetwsviewport(200, &wsv);
 
-        w.xmin = 0.0;
-        w.xmax = 1.0;
-        w.ymin = 0.0;
-        w.ymax = 1.0;
-        gsetwindow(1, &w);
+    w.xmin = 0.0;
+    w.xmax = 1.0;
+    w.ymin = 0.0;
+    w.ymax = 1.0;
+    gsetwindow(1, &w);
 
-        v.xmin = 0.0;
-        v.xmax = 1.0;
-        v.ymin = 0.0;
-        v.ymax = 0.8;
-        gsetviewport(1, &v);
+    v.xmin = 0.0;
+    v.xmax = 1.0;
+    v.ymin = 0.0;
+    v.ymax = 0.8;
+    gsetviewport(1, &v);
 
-        gsetwswindow(ws_id, &wsw);
-        gsetwsviewport(ws_id, &wsv);
+    gsetwswindow(ws_id, &wsw);
+    gsetwsviewport(ws_id, &wsv);
 
-        gselntran(1);
-        test_font();
+    gselntran(1);
+    test_font();
 
-        for(i=1; i<10; i++) {
-                getchar();
-                gclearws(200, GALWAYS);
-                gclearws(1, GALWAYS);
-                show_font(i);
-        }
+    for (i = 1; i < 10; i++)
+    {
+        getchar();
+        gclearws(200, GALWAYS);
+        gclearws(1, GALWAYS);
+        show_font(i);
+    }
 
-        fprintf(stderr,"Done, press break to quit ...\n"); getchar();
+    fprintf(stderr, "Done, press break to quit ...\n");
+    getchar();
 
-        WaitForBreak (1);
+    WaitForBreak(1);
 
-        if ((result = gdeactivatews(200)) != 0)
-                perr(result, "...deactivate_ws");
+    if ((result = gdeactivatews(200)) != 0)
+        perr(result, "...deactivate_ws");
 
-        if ((result = gclosews(200)) != 0)
-                perr(result, "...close_ws");
+    if ((result = gclosews(200)) != 0)
+        perr(result, "...close_ws");
 
-        if ((result = gdeactivatews(ws_id)) != 0)
-                perr(result, "...deactivate_ws");
+    if ((result = gdeactivatews(ws_id)) != 0)
+        perr(result, "...deactivate_ws");
 
-        if ((result = gclosews(ws_id)) != 0)
-                perr(result, "...close_ws");
+    if ((result = gclosews(ws_id)) != 0)
+        perr(result, "...close_ws");
 
-        if ((result = gclosegks()) != 0)
-                perr(result,"...close_gks");
-        fprintf(stdout,"after close_gks\n");
-        exit(0);
+    if ((result = gclosegks()) != 0)
+        perr(result, "...close_gks");
+    fprintf(stdout, "after close_gks\n");
+    exit(0);
 }
 
 void perr(int i, char *s)
 {
-        fprintf(stdout,"%s %d\n",s,i);
-        exit(1);
+    fprintf(stdout, "%s %d\n", s, i);
+    exit(1);
 }
 
 Gasfs asf = {
-        GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL,          /* polyline */
-        GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL,          /* polymarker */
-        GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL,     /* text */
-        GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL           /* fillarea */
+    GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL,              /* polyline */
+    GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL,              /* polymarker */
+    GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL, /* text */
+    GINDIVIDUAL, GINDIVIDUAL, GINDIVIDUAL               /* fillarea */
 };
 
 void test_font(void)
 {
-        Gpoint tpt;
-        char s[100];
-        int i;
-        Gpoint up;
-        Gtxfp txfp;
-        Gtxalign align;
+    Gpoint tpt;
+    char s[100];
+    int i;
+    Gpoint up;
+    Gtxfp txfp;
+    Gtxalign align;
 
-        txfp.font = 4;
+    txfp.font = 4;
+    txfp.prec = GSTROKE;
+    gsettextfontprec(&txfp);
+    gsetcharexpan(0.5);
+    gsetcharspace(0.2);
+    gsettextcolorind(WHITE); /* WHITE */
+
+    gsetcharheight(0.05);
+    up.x = 0.0;
+    up.y = 1.0;
+    gsetcharup(&up);
+    align.hor = GTH_CENTER;
+    align.ver = GTV_BASE;
+    gsettextalign(&align);
+    gsettextpath(GTP_RIGHT);
+
+    gsetasf(&asf);
+
+    tpt.x = 0.5;
+    tpt.y = 0.9;
+    gtext(&tpt, "GKS VECTOR FONTS");
+
+    gsetcharheight(0.05);
+    align.hor = GTH_LEFT;
+    align.ver = GTV_BASE;
+    gsettextalign(&align);
+
+    tpt.x = 0.01;
+    tpt.y = 0.90;
+    for (i = 1; i < 10; i++)
+    {
+        txfp.font = i;
         txfp.prec = GSTROKE;
         gsettextfontprec(&txfp);
-        gsetcharexpan(0.5);
-        gsetcharspace(0.2);
-        gsettextcolorind(WHITE);                /* WHITE */
-
-        gsetcharheight(0.05);
-        up.x = 0.0; up.y = 1.0;
-        gsetcharup(&up);
-        align.hor = GTH_CENTER;
-        align.ver = GTV_BASE;
-        gsettextalign(&align);
-        gsettextpath(GTP_RIGHT);
-
-        gsetasf(&asf);
-
-        tpt.x = 0.5; tpt.y = 0.9;
-        gtext(&tpt,"GKS VECTOR FONTS");
-
-        gsetcharheight(0.05);
-        align.hor = GTH_LEFT;
-        align.ver = GTV_BASE;
-        gsettextalign(&align);
-
-        tpt.x = 0.01; tpt.y = 0.90;
-        for(i=1; i<10; i++) {
-                txfp.font = i;
-                txfp.prec = GSTROKE;
-                gsettextfontprec(&txfp);
-                SetColor(i);
-                tpt.y -= 0.08;
-                sprintf(s,"Font #%d  AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz",i);
-                gtext(&tpt, s);
-        }
+        SetColor(i);
+        tpt.y -= 0.08;
+        sprintf(s, "Font #%d  AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz", i);
+        gtext(&tpt, s);
+    }
 }
 
 void show_font(int font)
 {
-        Gpoint tpt;
-        char s[100];
-        Gpoint up;
-        Gtxfp txfp;
-        Gtxalign align;
+    Gpoint tpt;
+    char s[100];
+    Gpoint up;
+    Gtxfp txfp;
+    Gtxalign align;
 
-        txfp.font = 1;
-        txfp.prec = GSTROKE;
-        gsettextfontprec(&txfp);
+    txfp.font = 1;
+    txfp.prec = GSTROKE;
+    gsettextfontprec(&txfp);
 
-        gsetcharexpan(0.5);
-        gsetcharspace(0.2);
-        gsettextcolorind(WHITE);                /* WHITE */
+    gsetcharexpan(0.5);
+    gsetcharspace(0.2);
+    gsettextcolorind(WHITE); /* WHITE */
 
-        gsetcharheight(0.05);
-        up.x = 0.0; up.y = 1.0;
-        gsetcharup(&up);
-        align.hor = GTH_CENTER;
-        align.ver = GTV_BASE;
-        gsettextalign(&align);
-        gsettextpath(GTP_RIGHT);
+    gsetcharheight(0.05);
+    up.x = 0.0;
+    up.y = 1.0;
+    gsetcharup(&up);
+    align.hor = GTH_CENTER;
+    align.ver = GTV_BASE;
+    gsettextalign(&align);
+    gsettextpath(GTP_RIGHT);
 
-        tpt.x = 0.5; tpt.y = 0.9;
-        sprintf(s,"GKS VECTOR FONT NUMBER %d",font);
-        gtext(&tpt, s);
+    tpt.x = 0.5;
+    tpt.y = 0.9;
+    sprintf(s, "GKS VECTOR FONT NUMBER %d", font);
+    gtext(&tpt, s);
 
-        gsetcharheight(0.05);
-        align.hor = GTH_LEFT;
-        align.ver = GTV_BASE;
-        gsettextalign(&align);
-        txfp.font = font;
-        txfp.prec = GSTROKE;
-        gsettextfontprec(&txfp);
+    gsetcharheight(0.05);
+    align.hor = GTH_LEFT;
+    align.ver = GTV_BASE;
+    gsettextalign(&align);
+    txfp.font = font;
+    txfp.prec = GSTROKE;
+    gsettextfontprec(&txfp);
 
-        tpt.x = 0.01; tpt.y = 0.80;
-        gtext(&tpt, " !\"#$%&'()*+,-./:;<=>?@");
-        tpt.y -= 0.08;
-        gtext(&tpt, "0123456789");
-        tpt.y -= 0.08;
-        gtext(&tpt, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        tpt.y -= 0.08;
-        gtext(&tpt, "abcdefghijklmnopqrstuvwxyz");
-        tpt.y -= 0.08;
-        gtext(&tpt, "[\\]^_`{|}~");
+    tpt.x = 0.01;
+    tpt.y = 0.80;
+    gtext(&tpt, " !\"#$%&'()*+,-./:;<=>?@");
+    tpt.y -= 0.08;
+    gtext(&tpt, "0123456789");
+    tpt.y -= 0.08;
+    gtext(&tpt, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    tpt.y -= 0.08;
+    gtext(&tpt, "abcdefghijklmnopqrstuvwxyz");
+    tpt.y -= 0.08;
+    gtext(&tpt, "[\\]^_`{|}~");
 }
 
-void SetColor (Gint hash)
+void SetColor(Gint hash)
 {
-        switch (hash) {
-                case 1 : gsettextcolorind(WHITE);   return;
-                case 2 : gsettextcolorind(RED);   return;
-                case 3 : gsettextcolorind(GREEN);   return;
-                case 4 : gsettextcolorind(BLUE);   return;
-                case 5 : gsettextcolorind(YELLOW);   return;
-                case 6 : gsettextcolorind(CYAN);   return;
-                case 7 : gsettextcolorind(SILVER);   return;
-                case 8 : gsettextcolorind(DARKGREEN);   return;
-                case 9 : gsettextcolorind(BEIGE);   return;
-        }
+    switch (hash)
+    {
+    case 1:
+        gsettextcolorind(WHITE);
+        return;
+    case 2:
+        gsettextcolorind(RED);
+        return;
+    case 3:
+        gsettextcolorind(GREEN);
+        return;
+    case 4:
+        gsettextcolorind(BLUE);
+        return;
+    case 5:
+        gsettextcolorind(YELLOW);
+        return;
+    case 6:
+        gsettextcolorind(CYAN);
+        return;
+    case 7:
+        gsettextcolorind(SILVER);
+        return;
+    case 8:
+        gsettextcolorind(DARKGREEN);
+        return;
+    case 9:
+        gsettextcolorind(BEIGE);
+        return;
+    }
 }
